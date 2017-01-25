1fbff8acfe9f9ab7c01559057f13157fc331c773
Rodrigo Lopez-Romero <rlromero@gmv.com>
2017-01-25 11:07:09 +0100

    Adding the view controllers for the list of locations and for the map.

---
+++ On the map/ListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  listViewController.swift
+//  On the map
+//
+//  Created by Gmv100 on 25/01/2017.
+//  Copyright © 2017 GMV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
